b7c2ca239c4619ba14decfb934f70453a5881bda
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-11 16:37:52 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solutio
+    var etalon = null;
+    var strit = str.split('');
+    var result = checkBrackets();
+    function checkBrackets(){
+        var checkA = 0;
+        var checkB = 0;
+        for( var i = 0; i < strit.length; i++){
+            checkA = strit[i];
+            checkB = strit[i + 1];
+            for( var k=0; k < bracketsConfig.length; k++){
+                if(checkA == bracketsConfig[k][0] && checkB == bracketsConfig[k][1]){
+                    strit.splice(i,2);
+                    checkBrackets();
+                }
+            }
+        }
+        if(strit.length == 0){
+                return true;
+            }else if(etalon == strit){
+                return false;
+            }else{ 
+                etalon = strit;
+                return checkBrackets();
+            }
+    }
+   return result;
 }
